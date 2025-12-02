320bfe7320c1859a48f6782e787432367f3e44d4
ghifar1327 <givartorreto@gmail.com>
2025-12-02 23:28:53 +0700

    feat: adds button reset

--- src/pages/RickAndMorty.jsx
+++ src/pages/RickAndMorty.jsx
@@ -28,21 +28,29 @@ export default function RickAndMorty() {
   const filtered = cards.filter((item) =>
     item.name.toLowerCase().includes(keyword.toLowerCase())
   );
+  const handleReset = () => {
+    setSearch("");
+    setKeyword("");
+  };
 
   return (
     <>
       <header className="text-center text-6xl font-bold">Rick And Morty</header>
       <main className="p-10">
-        <form action="" onSubmit={handleSubmit}>
+        <form action="" onSubmit={handleSubmit} className="flex justify-center gap-2 mb-5">
           <input
             type="text"
             name="search"
             className="border"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
+            placeholder=" search character......"
           />
-          <button type="submit" className="border-2">
+          <button type="submit" className="border-2 px-2 rounded-md">
             search
+          </button>
+          <button type="reset" onClick={handleReset} className="border-2 px-2 rounded-md">
+            reset filter
           </button>
         </form>
         <CardBody cards={filtered} />
